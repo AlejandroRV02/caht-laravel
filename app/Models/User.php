e92b269e9aff7b16e5a6b7e7a1812e490ab54404
Alejandro <alejandrorv944@gmail.com>
2021-03-14 17:03:10 -0600

    Modelos creados

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,5 +46,8 @@ class User extends Authenticatable
         return $this->belongsToMany('App\Models\Chat');
     }
 
+    public function messages(){
+        return $this->hasMany('App\Models\Message');
+    }
 
 }
